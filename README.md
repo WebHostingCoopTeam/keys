0ec86fe04a76c2032f00d95503b76414b7eb9ef9
Josh Cox <josh@webhosting.coop>
2014-11-21 13:02:55 -0600

    added addus.sh

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ keys
 ====
 
 public keys for coop devops
+
+to addus to your server:
+
+sudo bash addus.sh
+
+but if you’re not a part of the coop then what are you doing this for?
---
+++ addus.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo 'This is for Webhosting.coop dev only, this will add our keys to your server, BEWARE!'
+touch /root/.ssh/authorized_keys
+cp /root/.ssh/authorized_keys /tmp/rootsshauthorized_keys
+curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/keys |xargs -n1 -I{} echo '{}'>> /tmp/rootsshauthorized_keys
+cat /tmp/rootsshauthorized_keys|sort|uniq>/tmp/rootsshauthorized_keysuniq
+rm /tmp/rootsshauthorized_keys
+echo 'I will be adding these entries (removing duplicates):'
+diff /tmp/rootsshauthorized_keysuniq /root/.ssh/authorized_keys
+echo -n 'ctrl-C to stop now! or forever hold your peace'
+sleep 1;echo -n '.';sleep 1;echo -n '.';sleep 1;echo -n '.';sleep 1;echo -n '.'
+sleep 1
+cp /tmp/rootsshauthorized_keysuniq /root/.ssh/authorized_keys
+rm /tmp/rootsshauthorized_keysuniq
