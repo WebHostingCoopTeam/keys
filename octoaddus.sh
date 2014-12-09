3597ec968bf93dd2edf65d6d81e167b2a7f6e157
Josh Cox <josh@webhosting.coop>
2014-12-09 08:16:59 -0600

    corrected the line issue

--- octoaddus.sh
+++ octoaddus.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/addus.sh | bash
 echo 'This is for Webhosting.coop dev only, this will add our keys to your server, BEWARE!'
-mkdir /tmp/home
 touch /home/git/.ssh/authorized_keys
-cp /home/git/.ssh/authorized_keys /tmp/home/gitsshauthorized_keys
-curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/gitreceive-keys |xargs -n1 -I{} echo '{}'>> /tmp/home/gitsshauthorized_keys
-cat /tmp/home/gitsshauthorized_keys|sort|uniq>/tmp/home/gitsshauthorized_keysuniq
-rm /tmp/home/gitsshauthorized_keys
+cp /home/git/.ssh/authorized_keys /tmp/gitsshauthorized_keys
+wget https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/gitreceive-keys -O /tmp/gitsshauthorized_keys.new
+cat /tmp/gitsshauthorized_keys.new >> /tmp/gitsshauthorized_keys
+rm /tmp/gitsshauthorized_keys.new
+cat /tmp/gitsshauthorized_keys|sort|uniq>/tmp/gitsshauthorized_keysuniq
+rm /tmp/gitsshauthorized_keys
 echo 'I will be adding these entries (removing duplicates):'
-diff /tmp/home/gitsshauthorized_keysuniq /home/git/.ssh/authorized_keys
+diff /tmp/gitsshauthorized_keysuniq /git/.ssh/authorized_keys
 echo -n 'ctrl-C to stop now! or forever hold your peace'
 sleep 1;echo -n '.';sleep 1;echo -n '.';sleep 1;echo -n '.';sleep 1;echo -n '.'
 sleep 1
-cp /tmp/home/gitsshauthorized_keysuniq /home/git/.ssh/authorized_keys
-rm /tmp/home/gitsshauthorized_keysuniq
+cp /tmp/gitsshauthorized_keysuniq /home/git/.ssh/authorized_keys
+rm /tmp/gitsshauthorized_keysuniq
+chown git. /home/git/.ssh/authorized_keys
+chmod 600 /home/git/.ssh/authorized_keys
