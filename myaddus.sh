d07d7039a22ec7108b0a344438a35e73dab77281
Josh Cox <josh@webhosting.coop>
2017-01-15 16:25:39 -0600

    silent

--- myaddus.sh
+++ myaddus.sh
@@ -10,7 +10,7 @@ echo 'This is for Webhosting.coop dev only, this will add our keys to your serve
 touch $HOME/.ssh/authorized_keys
 TMP=$(mktemp -d --suffix=SDX)
 cp $HOME/.ssh/authorized_keys $TMP/mysshauthorized_keys
-curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/keys |xargs -n1 -I{} echo '{}'>> $TMP/mysshauthorized_keys
+curl --silent https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/keys |xargs -n1 -I{} echo '{}'>> $TMP/mysshauthorized_keys
 cat $TMP/mysshauthorized_keys|sort|uniq>$TMP/mysshauthorized_keysuniq
 rm $TMP/mysshauthorized_keys
 diff $TMP/mysshauthorized_keysuniq $HOME/.ssh/authorized_keys
