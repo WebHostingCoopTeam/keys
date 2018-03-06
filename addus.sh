30f40ccb490ce2e0170e5fdc3843398cc1a49227
John Van Lowe <johnvanlowe@users.noreply.github.com>
2018-03-05 20:32:49 -0600

    mkdir -p prior
    for the new provisions that don't have /root/.ssh by default

--- addus.sh
+++ addus.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo 'This is for Webhosting.coop dev only, this will add our keys to your server, BEWARE!'
+mkdir -p /root/.ssh/
 touch /root/.ssh/authorized_keys
 cp /root/.ssh/authorized_keys /tmp/rootsshauthorized_keys
 curl https://raw.githubusercontent.com/WebHostingCoopTeam/keys/master/keys |xargs -n1 -I{} echo '{}'>> /tmp/rootsshauthorized_keys
